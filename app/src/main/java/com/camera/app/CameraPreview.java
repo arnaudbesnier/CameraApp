0525c17ba681755144b4a1ac6e29726f2e8f44c8
Arnaud Besnier <arnaudibesnier@gmail.com>
2014-04-23 18:38:18 +0200

    Clean code

--- app/src/main/java/com/camera/app/CameraPreview.java
+++ app/src/main/java/com/camera/app/CameraPreview.java
@@ -9,7 +9,9 @@ import android.view.SurfaceView;
 import java.io.IOException;
 
 public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
-    private static final String TAG = "ERROR";
+
+    private static final String TAG = "CameraApp";
+
     private SurfaceHolder mHolder;
     private Camera mCamera;
 
@@ -37,7 +39,7 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
             mCamera.setPreviewDisplay(holder);
             mCamera.startPreview();
         } catch (IOException e) {
-            Log.d(TAG, "Error setting camera preview: " + e.getMessage());
+            Log.e(TAG, "Error setting camera preview: " + e.getMessage());
         }
     }
 
@@ -46,31 +48,7 @@ public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback
     }
 
     public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-        // If your preview can change or rotate, take care of those events here.
-        // Make sure to stop the preview before resizing or reformatting it.
-
-        if (mHolder.getSurface() == null) {
-            // preview surface does not exist
-            return;
-        }
-
-        // stop preview before making changes
-        try {
-            mCamera.stopPreview();
-        } catch (Exception e){
-            // ignore: tried to stop a non-existent preview
-        }
-
-        // set preview size and make any resize, rotate or
-        // reformatting changes here
-
-        // start preview with new settings
-        try {
-            mCamera.setPreviewDisplay(mHolder);
-            mCamera.startPreview();
-
-        } catch (Exception e){
-            //Log.d(TAG, "Error starting camera preview: " + e.getMessage());
-        }
+        // We should not enter in this method
+        Log.e(TAG, "Error in surfaceChanged: we should not enter here");
     }
 }
